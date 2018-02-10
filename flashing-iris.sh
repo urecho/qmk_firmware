537f977672d98d1ebfae1503ab23936f4366ccce
Yoonseok Woo <urecho@gmail.com>
2018-02-10 20:27:59 +0900

    urecho's personal keymap

---
+++ flashing-iris.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -ex
+
+# install_name_tool -change '/usr/local/opt/isl/lib/libisl.21.dylib' /usr/local/opt/isl/lib/libisl.dylib /usr/local/Cellar/avr-gcc@8/8.3.0/libexec/gcc/avr/8.3.0/cc1
+# make git-submodule
+make keebio/iris/rev2:urecho \
+ && sudo make keebio/iris/rev2:urecho:flash
+
