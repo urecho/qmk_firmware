b94d7efc130cf71d4fafacb4c2021842af867092
Yoonseok Woo <urecho@gmail.com>
2021-05-16 22:42:53 +0900

    kyria

--- keyboards/splitkb/kyria/rules.mk
+++ keyboards/splitkb/kyria/rules.mk
@@ -2,7 +2,19 @@
 MCU = atmega32u4
 
 # Bootloader selection
-BOOTLOADER = atmel-dfu
+# BOOTLOADER = atmel-dfu
+#   Teensy       halfkay
+#   Pro Micro    caterina
+#   Atmel DFU    atmel-dfu
+#   LUFA DFU     lufa-dfu
+#   QMK DFU      qmk-dfu
+#   ATmega32A    bootloadHID
+#   ATmega328P   USBasp
+
+# elite-c
+BOOTLOADER = qmk-dfu
+# pro micro
+#BOOTLOADER = caterina
 
 # Build Options
 #   change yes to no to disable
@@ -15,6 +27,9 @@ COMMAND_ENABLE = no        # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
 AUDIO_ENABLE = no          # Audio output
+MIDI_ENABLE = no           # MIDI support
+UNICODE_ENABLE = no        # Unicode
+BLUETOOTH_ENABLE = no      # Enable Bluetooth with the Adafruit EZ-Key HID
 SPLIT_KEYBOARD = yes       # Split common
 LTO_ENABLE = yes
 
